3e3f743f52b73a8160ef50163d3fdcda8c482d24
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2026-01-16 13:42:22 +0530

    Delete route.js

--- src/app/api/test-db/route.js
+++
@@ -1,9 +0,0 @@
-// File: src/app/api/test-db/route.js
-
-import connectDB from '@/lib/db/mongodb';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  await connectDB();
-  return NextResponse.json({ status: 'MongoDB connected ✅' });
-}
